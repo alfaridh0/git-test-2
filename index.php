ec82f034fc81fcc2685fe8b14ae49c71773ca378
Niken Alfrido <alfrisiboro@gmail.com>
2022-06-17 19:44:49 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit at ut alias error ipsum temporibus, delectus optio recusandae reiciendis consequatur rem esse unde neque perferendis, deserunt quas non ducimus? Iure.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
